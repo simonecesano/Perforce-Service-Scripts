c74773e370a5047b4fbe6c3b572f5b1c5186cdf2
simone cesano <simone.cesano@gmail.com>
2014-08-16 00:37:10 +0200

    fixed p4 to point to a generic location

--- login.sh
+++ login.sh
@@ -1,18 +1,18 @@
-# passwd=`security find-generic-password -ws Exchange 2>&1 | sed s/\n//g`
-# echo $passwd;
-# read -r -d '' foo <<'EOF'
+export P4PORT=10.127.22.43:1666
+passwd=`security find-generic-password -ws Exchange | /usr/bin/perl -pe "s/\n//g"`
 
-expect <<'EOF'
-set timeout 20
+whoami=`whoami`
+p4bin="/Users/"$whoami"/bin/p4"
 
-spawn p4 login
+/usr/bin/expect -c "
+spawn $p4bin login;
+expect \"Enter password:\";
+send \"$passwd\r\"; 
+expect eof
+exit
+"
+res=`$p4bin login -s`
 
-expect "Enter password:"
-
-send "sindbad11\\r"
-
-interact
-
+osascript <<EOF
+tell app "System Events" to display dialog "$res" buttons "OK" default button 1
 EOF
-
-echo "$foo"
