c7ad1577db2656fd33b40ee5d5bc94a24343b5cc
simone cesano <simone.cesano@gmail.com>
2014-08-16 00:37:03 +0200

    fixed p4 to point to a generic location

--- checkin.sh
+++ checkin.sh
@@ -1,15 +1,16 @@
 export P4PORT=10.127.22.43:1666
+whoami=`whoami`
+P4BIN="/Users/"$whoami"/bin/p4"
+
 description=$(/usr/bin/osascript <<-EOF
 tell application "Finder"
-    repeat while true
-        set input to display dialog "Enter the change description:" default answer ""
-        if button returned of input is equal to "OK" then
-            try
-                return (text returned of input) 
-            end try
-        end if
-    end repeat
+     set input to display dialog "Enter the change description:" default answer "" buttons {"OK", "cancel"}
+     if button returned of input is equal to "OK" then
+     	return (text returned of input) 
+     end if
 end tell
 EOF
 )
-find "$1" -type f -print -exec /Users/cesansim/bin/p4 submit -d "$description" {} \; >> /Users/cesansim/pony.txt 2>&1
+# needs to check for cancellation
+# needs to check that there is a description
+find "$1" -type f -print -exec $P4BIN submit -d "$description" {} \; >> /Users/"$whoami"/pony.txt 2>&1
