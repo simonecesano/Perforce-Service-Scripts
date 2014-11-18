8b81173bed7ee455fec37016d527cb4c7139b853
simone cesano <simone.cesano@gmail.com>
2014-11-18 11:28:58 +0100

    Adding service-to-zip workflow

---
+++ tools/pack_and_zip.pl
@@ -0,0 +1,25 @@
+use FindBin;
+
+$\ = "\n";
+
+my $service = $ARGV[0];
+
+my $whoami = qx/whoami/; $whoami =~ s/\s$//;
+my $service_folder = join '/', '/Users', $whoami, 'Library/Services';
+
+
+print $service;
+print join '/', $service_folder;
+print join '/', $service_folder, $service;
+
+$zipfile = $service;
+for ($zipfile) {
+    $_ = lc;
+    s/\.workflow$/.zip/;
+    s/\s+/_/g;
+}
+
+my $bin = $FindBin::Bin;
+my $outfile = join '/', $bin, '../zips', $zipfile;
+chdir $service_folder;
+qx/zip -r "$outfile" "$service"/;
