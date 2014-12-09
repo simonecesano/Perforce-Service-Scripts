ce2f11830b2881bd1050759c866f8011c2778f66
simone cesano <simone.cesano@gmail.com>
2014-12-09 15:03:56 +0100

    general cleanup

--- tools/pack_and_zip.pl
+++ tools/pack_and_zip.pl
@@ -7,7 +7,6 @@ my $service = $ARGV[0];
 my $whoami = qx/whoami/; $whoami =~ s/\s$//;
 my $service_folder = join '/', '/Users', $whoami, 'Library/Services';
 
-
 print $service;
 print join '/', $service_folder;
 print join '/', $service_folder, $service;
@@ -22,6 +21,6 @@ for ($zipfile) {
 my $bin = $FindBin::Bin;
 my $outfile = join '/', $bin, '../zips', $zipfile;
 chdir $service_folder;
-qx/zip -r "$outfile" "$service"/;
+# qx/zip -r "$outfile" "$service"/;
 
 # https://github.com/simonecesano/Perforce-Service-Scripts/raw/master/zips/login_to_desi.zip
